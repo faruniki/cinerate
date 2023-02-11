344d9f7b0f552256752921b3abcd4697786fec45
faruniki <najibfahrunaakbar123@gmail.com>
2023-02-12 05:02:54 +0700

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.admin')
 
-@section('title', 'Users')
+@section('title', 'Dashboard')
 @section('content')
+
+<link rel="stylesheet" href="assets/css/dashboard.css">
 
     <link rel="stylesheet" href="assets/css/dashboard.css">
 
-    
+    <div class="containernya">
+        <div class="kiri">
+            <p class="juduldash">Users</p>
+            <p class="totaldash">3</p>
+        </div>
+        <div class="tengah">
+            <p class="juduldash">Movies</p>
+            <p class="totaldash">13</p>
+        </div>
+        <div class="kanan">
+            <p class="juduldash">Reviews</p>
+            <p class="totaldash">4</p>
+        </div>
+    </div>
 
 @endsection
